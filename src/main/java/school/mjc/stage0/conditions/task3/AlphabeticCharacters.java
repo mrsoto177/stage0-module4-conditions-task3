211d53353444bf9440f162aab18304259ea104be
Marcos <msoto@hopewellsystem.com>
2024-02-25 22:05:40 -0600

    m4 cond t3 5

--- src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
+++ src/main/java/school/mjc/stage0/conditions/task3/AlphabeticCharacters.java
@@ -4,14 +4,19 @@ public class AlphabeticCharacters {
     public void vowelDeterminer(char character) {
     	if ( character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u' ) {
     		System.out.println("Vowel");
-    	} else {
+    	} else if ((character >= 'a' && character <= 'z')) {
+    		
     		System.out.println("Consonant");
+    	}
+    	else {
+    		System.out.println("It is not a number");
     	}
     }
     
     public static void main(String[] args) {
     	AlphabeticCharacters determiner = new AlphabeticCharacters();
-    	determiner.vowelDeterminer('r');
+    	determiner.vowelDeterminer('t');
     	determiner.vowelDeterminer('a');
+    	determiner.vowelDeterminer('2');
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -4,14 +4,15 @@ public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
     	if ((symbol >= 'a' && symbol <= 'z') || (symbol >= 'A' && symbol <= 'Z')) {
             System.out.println("English");
-        } else {
+        } else if ((symbol != 'a' && symbol != 'z') || (symbol != 'A' && symbol != 'Z')) {
             System.out.println("Non English");
-        }
+        } 
     }
     
     public static void main(String[] args) {
     	IsEnglishSymbolDeterminer symbol = new IsEnglishSymbolDeterminer();
     	symbol.isEnglishSymbol('{');
     	symbol.isEnglishSymbol('e');
+    	symbol.isEnglishSymbol('2');
     }
 }
